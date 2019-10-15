8ef8718afc9d1d6eb705bf44d0c2aed688860fda
Roger-luo <hiroger@qq.com>
2019-10-15 01:26:21 -0400

    Files generated by PkgTemplates

---
+++ src/JuliaGuide.jl
@@ -0,0 +1,5 @@
+module JuliaGuide
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using JuliaGuide
+using Test
+
+@testset "JuliaGuide.jl" begin
+    # Write your own tests here.
+end
